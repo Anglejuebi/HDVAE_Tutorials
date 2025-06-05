d64d645448ece92c62e6d0def1c769cd27c9150e
絕筆 <80617504+Anglejuebi@users.noreply.github.com>
2025-06-06 00:10:50 +0800

    Add files via upload

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,9 +1,5 @@
 # Configuration file for the Sphinx documentation builder.
 
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../..'))
-
 # -- Project information
 project = 'HDVAE Tutorials'
 copyright = '2025, JunHua Yu'
@@ -12,32 +8,19 @@ release = '1.0'
 
 # -- General configuration
 extensions = [
-   'nbsphinx',  # Render .ipynb files
-   'sphinx.ext.autodoc',
-   'sphinx.ext.doctest',
-   'sphinx.ext.intersphinx',
-   'sphinx_rtd_theme',  # Read the Docs theme
+   'nbsphinx',
 ]
 
 # Support for file suffixes
-source_suffix = ['.rst', '.ipynb']
+source_suffix = ['.ipynb']
 
 # nbsphinx configuration
 nbsphinx_allow_errors = True
-nbsphinx_execute = 'never'  # Skip executing notebooks to speed up builds
+nbsphinx_execute = 'never'
 
 # HTML output configuration
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
 templates_path = ['_templates']
 
 # Language setting
 language = 'en'  # English
-
-# Workaround for nbsphinx parser registration on Sphinx 6.x+
-import nbsphinx
-from sphinx.application import Sphinx
-from sphinx.util.docutils import docutils_namespace
-
-# def setup(app: Sphinx):
-#     nbsphinx.setup(app)  # 手动调用 nbsphinx 的注册
